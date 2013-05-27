50e6542f70455ddf3021e0f7c8e06bd45513a85b
Alexander Scheurer <alexander.scheurer@hs-furtwangen.de>
2013-05-27 11:47:28 +0200

    Physics ... it works

---
+++ Test2/Assets/Shoot.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnMouseDown() {
+		rigidbody.AddForce(0,0,-2000);
+		rigidbody.useGravity = true;
+	}
+}
